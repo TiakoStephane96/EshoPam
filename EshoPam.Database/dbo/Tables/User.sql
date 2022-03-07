576f76d2d1fc60a61a27bb21e221b9ee1708b3c4
Stéphane Tiako <Stphane Tiako@DESKTOP-SGS67NR>
2022-03-08 00:17:21 +0100

    Create users table   done: #6 create table category done: #1

---
+++ EshoPam.Database/dbo/Tables/User.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[User] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [Username] NVARCHAR (50) NOT NULL,
+    [Password] VARCHAR (100) NOT NULL,
+    [Fullname] NVARCHAR (50) NOT NULL,
+    [Role]     VARCHAR (50)  NOT NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [IX_User_1] UNIQUE NONCLUSTERED ([Username] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_User]
+    ON [dbo].[User]([Id] ASC);
+
