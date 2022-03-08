e2f3bc125b442bb9380687ab0bcb88f4814a0f41
Stéphane Tiako <Stphane Tiako@DESKTOP-SGS67NR>
2022-03-08 02:40:33 +0100

    Repository User : #7 and category : #2 is done

---
+++ EshoPam.Repository/CategoryRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EshoPam.Repository
+{
+    class CategoryRepository
+    {
+        private readonly EshopamEntities db;
+
+        public CategoryRepository()
+        {
+            db = new EshopamEntities();
+        }
+
+        public Category Get(int id)
+        {
+            return db.Categories.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Category Get(string name)
+        {
+            return db.Categories.FirstOrDefault(x => x.Name == name);
+        }
+
+        public Category Set(Category category)
+        {
+            if (category == null)
+                throw new ArgumentNullException(nameof(category));
+
+            var oldCategorie = new EshopamEntities().Users.Find(category.Id);
+
+            if (oldCategorie == null)
+                throw new KeyNotFoundException($"Category not found !");
+
+            var cat = Get(category.Name);
+
+            if (cat != null && cat.Id != oldCategorie.Id)
+                throw new DuplicateWaitObjectException($"Category name {nameof(category.Name)} already exist !");
+
+            db.Entry(category).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return category;
+        }
+
+        public Category Add(Category category)
+        {
+            if (category == null)
+                throw new ArgumentNullException(nameof(category));
+
+            var categorie = Get(category.Name);
+            if (categorie != null)
+                throw new DuplicateWaitObjectException($"Category name {nameof(category.Name)} already exist !");
+
+            category = db.Categories.Add(category);
+            db.SaveChanges();
+
+            return category;
+        }
+
+        public Category Delete(int id)
+        {
+            var category = Get(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return category;
+        }
+
+        public IEnumerable<Category> Find(Func<Category,bool> predicate)
+        {
+            return db.Categories.Where(predicate).ToArray();
+        }
+    }
+}
